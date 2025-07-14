affef9dde64f1b15431a380c095ac7f6cf3d2b0c
IB2357 <ibrahim.off55555@gmail>
2025-07-14 20:51:46 +0300

    test: two tests for driver full name

--- rentals/rentals/doctype/driver/test_driver.py
+++ rentals/rentals/doctype/driver/test_driver.py
@@ -1,9 +1,37 @@
 # Copyright (c) 2025, Ibrahim Sultan and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
+# class TestDriver(FrappeTestCase):
+	
+# 	def test_driver_full_name(self):
+# 		pass 
+
+# 	def test_driver_full_name_if_last_name_not_set(self):
+# 		pass
+
 class TestDriver(FrappeTestCase):
-	pass
+    
+    def test_driver_full_name(self):
+        driver = frappe.get_doc({
+            "doctype": "Driver",
+            "first_name": "John",
+            "last_name": "Doe",
+            "license_number":"TASNEEM"
+        })
+        driver.insert()
+
+        self.assertEqual(driver.full_name, "John Doe")
+
+    def test_driver_full_name_if_last_name_not_set(self):
+        driver = frappe.get_doc({
+            "doctype": "Driver",
+            "first_name": "Alice",
+            "license_number":"TASNEEM"
+        })
+        driver.insert()
+
+        self.assertEqual(driver.full_name, "Alice")
